3a57e25aa2b23ef480d6ce183f6af8051655a3b1
Scott Young <young.scottw@gmail.com>
2017-01-29 10:32:32 -0600

    Passes all tests

--- app/controllers/figures_controller.rb
+++ app/controllers/figures_controller.rb
@@ -3,8 +3,49 @@ class FiguresController < ApplicationController
     erb :'/figures/new'
   end
 
-  post '/figures' do
-    puts params
-    erb :"/figures/#{@figure.slug}"
+  get '/figures/:id/edit' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/edit'
   end
+
+  get '/figures/:id' do
+    @figure = Figure.find_by_id(params[:id])
+    erb :'/figures/show'
+  end
+
+  get '/figures' do
+    @figures = Figure.all
+    erb :'/figures/index'
+  end
+
+  post '/figures/:id' do
+    #binding.pry
+    @figure = Figure.find(params[:id])
+    @figure.update(params[":figure"])
+
+    if !params["landmark"]["name"].empty?
+      @figure.landmarks << Landmark.create(params[:landmark])
+    end
+
+    if !params["title"]["name"].empty?
+      @figure.titles << Title.create(params[:title])
+    end
+
+    @figure.save
+    erb :'/figures/show'
+  end
+
+  post '/figures' do
+    @figure = Figure.create("name" => params["figure"]["name"])
+    @title = Title.find_or_create_by("name" => params["title"]["name"])
+    @landmark = Landmark.find_or_create_by("name" => params["landmark"]["name"], "year_completed" => params["landmark"]["year_completed"])
+    @figure.title_ids = params["figure"]["title_ids"]
+    @figure.titles << @title
+    @figure.landmark_ids = params["figure"]["landmark_ids"]
+    @figure.landmarks << @landmark
+    @figure.save
+    redirect '/figures'
+  end
+
+
 end
